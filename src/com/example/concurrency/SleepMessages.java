18a95ec65003147701bd1f140597508d8bf027f0
Chaitanya <chaitanya.t.kumar@gmail.com>
2024-05-24 11:35:07 +0530

    pausin execution with sleep

--- src/com/example/concurrency/SleepMessages.java
+++ src/com/example/concurrency/SleepMessages.java
@@ -1,4 +1,11 @@
 package com.example.concurrency;
 
 public class SleepMessages {
+    public static void main(String[] args) throws InterruptedException {
+        String[] importantInfo = {"sa re ga me","pa da ni se","marchi veysey","that's what we say"};
+        for (int i=0;i<importantInfo.length;i++){
+            Thread.sleep(4000); // pause 4 seconds
+            System.out.println(importantInfo[i]); // print message
+        }
+    }
 }
